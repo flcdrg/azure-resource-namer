bd401c423d083265348bf0994099dbba707f8907
David Gardiner <david@gardiner.net.au>
2021-08-10 21:14:34 +0930

    Generate region

---
+++ Update-Region.ps1
@@ -0,0 +1,31 @@
+
+function Generate {
+  
+
+$grouped = az account list-locations | ConvertFrom-Json -Depth 5 | Where-Object { $_.metadata.regionType -eq "Physical" } | Group-Object { $_.metadata.geographyGroup }
+
+
+'<template>
+  <label for="region" class="form-label">Region</label>
+  <select class="form-control" id="region" value.bind="region">
+'
+
+  foreach ($group in $grouped) {
+    "    <optgroup label=`"$($group.Name)`">"
+    
+    foreach ($region in ($group.Group | Sort-Object -Property Name)) {
+
+      "      <option value=`"$($region.name)`" title=`"$($region.metadata.physicalLocation)`">$($region.displayName)</option>"
+    }
+    "    </optgroup>"
+  }
+
+'  </select>
+</template>'
+
+}
+
+$content = Generate
+# # $grouped | ForEach-Object { "<optgroup label=`"$($_.Name)`">" + $_.Group | ForEach-Object { "  <option value=`"$($_.name)`">$($_.displayName)</option>" } + "</optgroup>" })
+
+Set-Content -Path .\src\region.html -Value $content
