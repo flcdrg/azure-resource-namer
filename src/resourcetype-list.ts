3a8c09f6d5fbdf5a80d8d7b826d6e3ea33e32aa2
David Gardiner <david@gardiner.net.au>
2021-09-14 20:13:00 +0930

    Fill out resource types

--- src/resourcetype-list.ts
+++ src/resourcetype-list.ts
@@ -1,41 +1,7 @@
 import { bindable, bindingMode } from 'aurelia-framework';
 
 export class ResourcetypeList {
-  resourceTypeGroups: {
-    name: string,
-    resources: 
-      {
-        name: string;
-        id: string;
-      }[]    
-  }[];
 
   @bindable({ defaultBindingMode: bindingMode.twoWay }) resource: string;
 
-  constructor() {
-    this.resourceTypeGroups = [
-      {
-        name: "General",
-        resources: [
-          {
-            name: "API Management",
-            id: "apim"
-          },
-          {
-            name: "Resource Group",
-            id: "rg"
-          }
-        ]
-      }, 
-      {
-        name: "Networking",
-        resources: [
-          {
-            name: "Application gateway",
-            id: "ag"
-          }
-        ]
-      }
-    ];
-  }
 }
