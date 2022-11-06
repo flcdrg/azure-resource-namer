ef188641c4f26b8031c536de9a0c201022903c50
David Gardiner <david@gardiner.net.au>
2022-11-06 15:43:57 +1030

    Containers

--- src/resources.ts
+++ src/resources.ts
@@ -301,15 +301,25 @@ export const resources :
     assets: [
       {
         abbrev: 'aks',
-        name: 'AKS cluster'
+        name: 'AKS cluster',
+        regex: /^[a-zA-Z0-9]$|^[a-zA-Z0-9][-_a-zA-Z0-9]{0,61}[a-zA-Z0-9]$/ //https://learn.microsoft.com/en-us/rest/api/aks/managed-clusters/create-or-update?tabs=HTTP#uri-parameters
       },
       {
+        // https://learn.microsoft.com/en-us/azure/azure-resource-manager/management/resource-name-rules#microsoftcontainerregistry
         abbrev: 'cr',
-        name: 'Container registry'
+        name: 'Container registry',
+        minLength: 5,
+        maxLength: 50,
+        regex: /^[a-zA-Z0-9]*$/, // https://learn.microsoft.com/en-us/rest/api/containerregistry/registries/create?tabs=HTTP#uri-parameters
+        description: 'Alphanumerics'
       },
       {
+        // https://learn.microsoft.com/en-us/azure/azure-resource-manager/management/resource-name-rules#microsoftcontainerinstance
         abbrev: 'ci',
-        name: 'Container instance'
+        name: 'Container instance',
+        minLength: 1,
+        maxLength: 63,
+        description: 'Lowercase letters, numbers, and hyphens. Can\'t start or end with hyphen. Consecutive hyphens aren\'t allowed'
       },
       {
         abbrev: 'sf',
---
+++ test/unit/formatting/containers.spec.ts
@@ -0,0 +1,43 @@
+import { resources } from '../../../src/resources';
+import { formatResourceName } from "formatting";
+import { MockFeedback } from './MockFeedback';
+
+describe('formatting', () => {
+
+  describe('containers', () => {
+    let feedback: MockFeedback;
+
+    beforeEach( () => {
+      feedback = new MockFeedback();
+    });
+
+    describe('cr', () => {
+      const resource = resources.find(f => f.category == 'Containers').assets.find(a => a.abbrev === 'cr');
+
+      it('valid', () => {
+        const result = formatResourceName(
+          resource,
+          'workload',
+          'prod',
+          'eastus',
+          1,
+          feedback);
+    
+          expect(result).toMatchInlineSnapshot(`"cr-workload-prod-eastus-001"`);
+      });
+
+      it('invalid', () => {
+        formatResourceName(
+          resource,
+          'workloadwithinvalidchars!',
+          'prod',
+          'eastus',
+          1,
+          feedback);
+    
+          expect(feedback.resourceNameValid).toBe(false);
+      });
+    });
+
+  });
+})
