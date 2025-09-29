4362c614c347426aab4acbb352b9fdf0da3546bd
David Gardiner <david@gardiner.net.au>
2025-09-29 12:19:44 +0930

    Add new AI resources

--- src/resources.ts
+++ src/resources.ts
@@ -430,7 +430,39 @@ export const resources :
     assets: [
       {
         abbrev: 'srch',
-        name: 'Azure Cognitive Search'
+        name: 'AI Search'
+      },
+      {
+        abbrev: 'ais',
+        name: 'Azure AI services (multi-service account)',
+        // https://learn.microsoft.com/en-us/azure/templates/microsoft.cognitiveservices/accounts?pivots=deployment-language-bicep&WT.mc_id=DOP-MVP-5001655#microsoftcognitiveservicesaccounts
+        minLength: 2,
+        maxLength: 64,
+        regex: /^[a-zA-Z0-9][a-zA-Z0-9_.-]*$/
+      },
+      {
+        abbrev: 'aif',
+        name: 'Azure AI Foundry account',
+        // https://learn.microsoft.com/en-us/azure/templates/microsoft.cognitiveservices/accounts?pivots=deployment-language-bicep&WT.mc_id=DOP-MVP-5001655#microsoftcognitiveservicesaccounts
+        minLength: 2,
+        maxLength: 64,
+        regex: /^[a-zA-Z0-9][a-zA-Z0-9_.-]*$/
+      },
+      {
+        abbrev: 'proj',
+        name: 'Azure AI Foundry account project',
+        // https://learn.microsoft.com/azure/templates/microsoft.cognitiveservices/accounts/projects?pivots=deployment-language-bicep&WT.mc_id=DOP-MVP-5001655#microsoftcognitiveservicesaccountsprojects
+        minLength: 2,
+        maxLength: 64,
+        regex: /^[a-zA-Z0-9][a-zA-Z0-9_.-]*$/
+      },
+      {
+        abbrev: 'hub',
+        name: 'Azure AI Foundry hub'
+      },
+      {
+        abbrev: 'proj',
+        name: 'Azure AI Foundry hub project'
       },
       {
         abbrev: 'cog',
@@ -439,6 +471,119 @@ export const resources :
       {
         abbrev: 'mlw',
         name: 'Azure Machine Learning workspace'
+      },
+      {
+        abbrev: 'avi',
+        name: 'Azure AI Video Indexer'
+      },
+      {
+        abbrev: 'oai',
+        name: 'Azure OpenAI Service',
+        // https://learn.microsoft.com/en-us/azure/templates/microsoft.cognitiveservices/accounts?pivots=deployment-language-bicep&WT.mc_id=DOP-MVP-5001655#microsoftcognitiveservicesaccounts
+        minLength: 2,
+        maxLength: 64,
+        regex: /^[a-zA-Z0-9][a-zA-Z0-9_.-]*$/
+
+      },
+      {
+        abbrev: 'bot',
+        name: 'Bot service'
+      },
+      {
+        abbrev: 'cv',
+        name: 'Computer vision',
+        // https://learn.microsoft.com/en-us/azure/templates/microsoft.cognitiveservices/accounts?pivots=deployment-language-bicep&WT.mc_id=DOP-MVP-5001655#microsoftcognitiveservicesaccounts
+        minLength: 2,
+        maxLength: 64,
+        regex: /^[a-zA-Z0-9][a-zA-Z0-9_.-]*$/
+      },
+      {
+        abbrev: 'cm',
+        name: 'Content moderator',
+        // https://learn.microsoft.com/en-us/azure/templates/microsoft.cognitiveservices/accounts?pivots=deployment-language-bicep&WT.mc_id=DOP-MVP-5001655#microsoftcognitiveservicesaccounts
+        minLength: 2,
+        maxLength: 64,
+        regex: /^[a-zA-Z0-9][a-zA-Z0-9_.-]*$/
+      },
+      {
+        abbrev: 'cs',
+        name: 'Content safety',
+        // https://learn.microsoft.com/en-us/azure/templates/microsoft.cognitiveservices/accounts?pivots=deployment-language-bicep&WT.mc_id=DOP-MVP-5001655#microsoftcognitiveservicesaccounts
+        minLength: 2,
+        maxLength: 64,
+        regex: /^[a-zA-Z0-9][a-zA-Z0-9_.-]*$/
+      },
+      {
+        abbrev: 'cstv',
+        name: 'Custom vision (prediction)',
+        // https://learn.microsoft.com/en-us/azure/templates/microsoft.cognitiveservices/accounts?pivots=deployment-language-bicep&WT.mc_id=DOP-MVP-5001655#microsoftcognitiveservicesaccounts
+        minLength: 2,
+        maxLength: 64,
+        regex: /^[a-zA-Z0-9][a-zA-Z0-9_.-]*$/
+      },
+      {
+        abbrev: 'cstvt',
+        name: 'Custom vision (training)',
+        // https://learn.microsoft.com/en-us/azure/templates/microsoft.cognitiveservices/accounts?pivots=deployment-language-bicep&WT.mc_id=DOP-MVP-5001655#microsoftcognitiveservicesaccounts
+        minLength: 2,
+        maxLength: 64,
+        regex: /^[a-zA-Z0-9][a-zA-Z0-9_.-]*$/
+      },
+      {
+        abbrev: 'di',
+        name: 'Document intelligence',
+        // https://learn.microsoft.com/en-us/azure/templates/microsoft.cognitiveservices/accounts?pivots=deployment-language-bicep&WT.mc_id=DOP-MVP-5001655#microsoftcognitiveservicesaccounts
+        minLength: 2,
+        maxLength: 64,
+        regex: /^[a-zA-Z0-9][a-zA-Z0-9_.-]*$/
+      },
+      {
+        abbrev: 'face',
+        name: 'Face API',
+        // https://learn.microsoft.com/en-us/azure/templates/microsoft.cognitiveservices/accounts?pivots=deployment-language-bicep&WT.mc_id=DOP-MVP-5001655#microsoftcognitiveservicesaccounts
+        minLength: 2,
+        maxLength: 64,
+        regex: /^[a-zA-Z0-9][a-zA-Z0-9_.-]*$/
+      },
+      {
+        abbrev: 'hi',
+        name: 'Health Insights',
+        // https://learn.microsoft.com/en-us/azure/templates/microsoft.cognitiveservices/accounts?pivots=deployment-language-bicep&WT.mc_id=DOP-MVP-5001655#microsoftcognitiveservicesaccounts
+        minLength: 2,
+        maxLength: 64,
+        regex: /^[a-zA-Z0-9][a-zA-Z0-9_.-]*$/
+      },
+      {
+        abbrev: 'ir',
+        name: 'Immersive reader',
+        // https://learn.microsoft.com/en-us/azure/templates/microsoft.cognitiveservices/accounts?pivots=deployment-language-bicep&WT.mc_id=DOP-MVP-5001655#microsoftcognitiveservicesaccounts
+        minLength: 2,
+        maxLength: 64,
+        regex: /^[a-zA-Z0-9][a-zA-Z0-9_.-]*$/
+      },
+      {
+        abbrev: 'lang',
+        name: 'Language service',
+        // https://learn.microsoft.com/en-us/azure/templates/microsoft.cognitiveservices/accounts?pivots=deployment-language-bicep&WT.mc_id=DOP-MVP-5001655#microsoftcognitiveservicesaccounts
+        minLength: 2,
+        maxLength: 64,
+        regex: /^[a-zA-Z0-9][a-zA-Z0-9_.-]*$/
+      },
+      {
+        abbrev: 'spch',
+        name: 'Speech service',
+        // https://learn.microsoft.com/en-us/azure/templates/microsoft.cognitiveservices/accounts?pivots=deployment-language-bicep&WT.mc_id=DOP-MVP-5001655#microsoftcognitiveservicesaccounts
+        minLength: 2,
+        maxLength: 64,
+        regex: /^[a-zA-Z0-9][a-zA-Z0-9_.-]*$/
+      },
+      {
+        abbrev: 'trsl',
+        name: 'Translator',
+        // https://learn.microsoft.com/en-us/azure/templates/microsoft.cognitiveservices/accounts?pivots=deployment-language-bicep&WT.mc_id=DOP-MVP-5001655#microsoftcognitiveservicesaccounts
+        minLength: 2,
+        maxLength: 64,
+        regex: /^[a-zA-Z0-9][a-zA-Z0-9_.-]*$/
       },
     ]
   },
